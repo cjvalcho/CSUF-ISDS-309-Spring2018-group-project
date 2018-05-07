04deadc6410dd74d0f76e02224f96481b7a0824a
kassontruongCSU <36388815+kassontruongCSU@users.noreply.github.com>
2018-05-06 20:35:50 -0700

    Add files via upload

--- Spring2018_Group3_Project/Form2.cs
+++ Spring2018_Group3_Project/Form2.cs
@@ -19,6 +19,8 @@ namespace Spring2018_Group3_Project
         CarRegistration carRec;
         Customer c1 = new Customer();
 
+
+
         //Accepts a CarRegistration object from Form 1 and sets the read-only text boxes.
         public Form2(CarRegistration car)
         {
@@ -31,7 +33,169 @@ namespace Spring2018_Group3_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+         
+            // Fill arrays with values entered from appropriate TextBox
+            char[] arrayFirstName = tbFirstName.Text.ToArray();
+            char[] arrayLastName = tbLastName.Text.ToArray();
+            char[] arrayCity = tbCity.Text.ToArray();
+            char[] arrayState = tbState.Text.ToArray();
+            char[] arrayPhoneNumber = tbPhoneNumber.Text.ToArray();
+            char[] arrayZipcode = tbZipcode.Text.ToArray();
+            char[] arrayDriverID = tbDriverID.Text.ToArray();
+
+
+
+            if (tbFirstName.Text == "") // If TextBox first name is empty
+            {
+                MessageBox.Show("For FirstName please enter a value");
+                return; // stop program from running
+            }
+            else
+            {
+                for (int i = 0; i < arrayFirstName.Length; i++) //loop to check if Textbox array has a number
+                {
+                    if (arrayFirstName[i] == '1' || arrayFirstName[i] == '2' || arrayFirstName[i] == '3' || arrayFirstName[i] == '4' || arrayFirstName[i] == '5' 
+                        || arrayFirstName[i] == '6' || arrayFirstName[i] == '7' || arrayFirstName[i] == '8' || arrayFirstName[i] == '9' || arrayFirstName[i] == '0')
+                    {
+                        MessageBox.Show("For FirstName please do not enter any numbers"); // outputs if it finds a number
+                        return;
+                    }
+                }
+            }
+
+            if (tbLastName.Text == "") // If TextBox Last name is empty
+            {
+                MessageBox.Show("For LastName please enter a value");
+                return;
+            }
+
+            else
+            {
+                for (int i = 0; i < arrayLastName.Length; i++) //loop to check if Textbox array has a number
+                {
+                    if (arrayLastName[i] == '1' || arrayLastName[i] == '2' || arrayLastName[i] == '3' || arrayLastName[i] == '4' || arrayLastName[i] == '5' 
+                        || arrayLastName[i] == '6' || arrayLastName[i] == '7' || arrayLastName[i] == '8' || arrayLastName[i] == '9' || arrayLastName[i] == '0')
+                    {
+                        MessageBox.Show("For LastName please do not enter any numbers"); // outputs if it finds a number
+                        return;
+                    }
+                }
+            }
+
+            if (tbEmail.Text == "") // If TextBox email is empty
+            {
+                MessageBox.Show("For Email please enter a value");
+                return; // stop program from running
+            }
+
+            if (tbAddress.Text == "") // If TextBox address is empty
+            {
+                MessageBox.Show("For Address please enter a value");
+                return; // stop program from running
+            }
+
+            if (tbPhoneNumber.Text == "") // If TextBox PhoneNumber is empty
+            {
+                MessageBox.Show("For Phone Number please enter a value");
+                return;
+            }
+
+            else
+            {
+                for (int i = 0; i < arrayPhoneNumber.Length; i++) //loop to check if Textbox array does not have a number
+                {
+                    if (!(arrayPhoneNumber[i] == '1' || arrayPhoneNumber[i] == '2' || arrayPhoneNumber[i] == '3' || arrayPhoneNumber[i] == '4' || arrayPhoneNumber[i] == '5'
+                        || arrayPhoneNumber[i] == '6' || arrayPhoneNumber[i] == '7' || arrayPhoneNumber[i] == '8' || arrayPhoneNumber[i] == '9' || arrayPhoneNumber[i] == '-'
+                        || arrayPhoneNumber[i] == '(' || arrayPhoneNumber[i] == ')' || arrayPhoneNumber[i] == ' '))
+                    {
+                        MessageBox.Show("For Phone Number please do not enter any letters"); // outputs if it finds a number
+                        return;
+                    }
+                }
+            }
+
+            if (tbCity.Text == "") // If TextBox City is empty
+            {
+                MessageBox.Show("For City please enter a value");
+                return;
+            }
+
+            else
+            {
+                for (int i = 0; i < arrayCity.Length; i++) //loop to check if Textbox array has a number
+                {
+                    if (arrayCity[i] == '1' || arrayCity[i] == '2' || arrayCity[i] == '3' || arrayCity[i] == '4' || arrayCity[i] == '5' 
+                        || arrayCity[i] == '6' || arrayCity[i] == '7' || arrayCity[i] == '8' || arrayCity[i] == '9' || arrayCity[i] == '0')
+                    {
+                        MessageBox.Show("For City please do not enter any numbers"); // outputs if it finds a number
+                        return;
+                    }
+                }
+            }
+
+            if (tbState.Text == "") // If TextBox State is empty
+            {
+                MessageBox.Show("For State please enter a value");
+                return;
+            }
+
+            else
+            {
+                for (int i = 0; i < arrayState.Length; i++) //loop to check if Textbox array has a number
+                {
+                    if (arrayState[i] == '1' || arrayState[i] == '2' || arrayState[i] == '3' || arrayState[i] == '4' || arrayState[i] == '5' 
+                        || arrayState[i] == '6' || arrayState[i] == '7' || arrayState[i] == '8' || arrayState[i] == '9' || arrayState[i] == '0')
+                    {
+                        MessageBox.Show("For State please do not enter any numbers"); // outputs if it finds a number
+                        return;
+                    }
+                }
+            }
+
+
+            if (tbZipcode.Text == "") // If TextBox Zipcode is empty
+            {
+                MessageBox.Show("For Zipcode please enter a value");
+                return;
+            }
+
+            else
+            {
+                for (int i = 0; i < arrayZipcode.Length; i++) //loop to check if Textbox array does not have a number
+                {
+                    if (!(arrayZipcode[i] == '1' || arrayZipcode[i] == '2' || arrayZipcode[i] == '3' || arrayZipcode[i] == '4' || arrayZipcode[i] == '5' 
+                        || arrayZipcode[i] == '6' || arrayZipcode[i] == '7' || arrayZipcode[i] == '8' || arrayZipcode[i] == '9' || arrayZipcode[i] == '0'))
+                    {
+                        MessageBox.Show("For Zipcode please do not enter any letters"); // outputs if it finds a number
+                        return;
+                    }
+                }
+            }
+
+            if (tbDriverID.Text == "") // If TextBox Driver License is empty
+            {
+                MessageBox.Show("For Driver License number please enter a value");
+                return;
+            }
+
+         
+
             c1.dtDOB = dtpDOB.Value;
+            int iAge = DateTime.Today.Year - dtpDOB.Value.Year; // Calculate user age
+
+
+            //string strage = Convert.ToString(Age); // to see age
+            //MessageBox.Show(strage);
+
+            if (!(iAge >= 18))
+            {
+                MessageBox.Show("You are not 18 and are unable to drive a motorvehicle, according to US law"); // Display if user is underage
+                return;
+            }
+
+            //MessageBox.Show("all values passed");   //to test if values passed
+
             MessageBox.Show("Your DOB is " + c1.dtDOB.ToString("D"));
             // This will need error checking, and will call a function that will write things to a file. If successful, it will popup with a message box that says "Complete"
 
@@ -43,6 +207,7 @@ namespace Spring2018_Group3_Project
             const string SPACE = " ";
 
 
+        
 
             Customer cust = new Customer();
             // Open File Stream
@@ -72,6 +237,11 @@ namespace Spring2018_Group3_Project
             tbRateTotal.Text = (carRec.car.dblRate * Convert.ToDouble(carRec.iDays)).ToString("C");
             tbFees.Text = FEES.ToString("C");
             tbTotalCost.Text = ((carRec.car.dblRate * Convert.ToDouble(carRec.iDays)) + FEES).ToString("C");
+        }
+
+        private void tbFirstName_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
